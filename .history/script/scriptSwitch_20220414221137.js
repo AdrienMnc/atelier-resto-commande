00f8ef9b479813219aa3550021a13d4242201483
AdrienMnc <adrien.moncieu@gmail.com>
2022-05-28 18:06:50 +0200

    one coke plz

---
+++ .history/script/scriptSwitch_20220414221137.js
@@ -0,0 +1,84 @@
+//  variable / constante
+
+const PLAT = 20;
+const BOISSON_ALCOOLISEE = 8;
+const BOISSON_NON_ALCOOLISEE = 5;
+
+let age = prompt("Saisissez votre age:");
+console.log(age);
+let credit = prompt("Creditez votre compte");
+console.log(credit);
+
+function recupererChoixPossible(credit) {
+  if (credit >= 20) {
+    return "Que voulez vous commander : 1/ Plat 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else if (credit >= 8 && credit < 19) {
+    return "Que voulez vous commander : 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else if (credit >= 5 && credit < 8) {
+    return "Que voulez vous commander : 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else {
+    return "vous n'avez pas assez de credit";
+  }
+}
+// BOUCLE DEMANDE CHOIX CLIENT TANT QU IL Y A ARGENT
+let choix = "";
+while (credit >= 5 && choix != "4") {
+  choix = prompt(
+    "Que voulez vous commander : 1/ Plat 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir"
+  );
+  console.log(choix);
+switch (choix) {
+    case "1":
+        if (credit >= PLAT) {
+            credit = credit - PLAT;
+            console.log(
+              `Commande accepté voici votre plat il vous reste ${credit} euros`
+            );
+          } else {
+            console.log("Vous n'avez pas assez d'argent commandez autre chose.");
+          }
+        
+        break;
+    case "2":
+        if (age < 18) {
+            console.log(
+              "Vous n'êtes pas majeur vous ne pouvez pas commander d'alcool"
+            );
+          } else if (credit < BOISSON_ALCOOLISEE) {
+            console.log("Vas au travail pour gagner plus d'argent !");
+          } else {
+            credit = credit - BOISSON_ALCOOLISEE;
+            console.log(`A la tienne il vous reste ${credit} euros `);
+          }
+        break;
+
+    case "3":
+        if (credit >= BOISSON_NON_ALCOOLISEE) {
+            credit = credit - BOISSON_NON_ALCOOLISEE;
+            console.log(
+              `Commande accepté voici votre soda il vous reste ${credit} euros `
+            );
+          } else {
+            console.log("Vous n'avez pas assez d'argent commandez autre chose.");
+          }
+        break;
+
+    case "4":
+        console.log("Merci aurevoir");
+
+        break;
+    default:
+        break;
+}
+    
+
+ 
+
+
+
+  // TOUS LES AUTRES CAS
+  else {
+    console.log("Veuillez réessayer");
+  }
+  console.log(recupererChoixPossible(credit));
+}
---
+++ .history/script/script_20220415092037.js
@@ -0,0 +1,68 @@
+//  variable / constante
+
+const PLAT = 20;
+const BOISSON_ALCOOLISEE = 8;
+const BOISSON_NON_ALCOOLISEE = 5;
+
+let age = prompt("Saisissez votre age:");
+alert(age);
+let credit = prompt("Creditez votre compte");
+alert(credit);
+
+function recupererChoixPossible(credit) {
+  if (credit >= 20) {
+    return "Que voulez vous commander : 1/ Plat 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else if (credit >= 8 && credit < 19) {
+    return "Que voulez vous commander : 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else if (credit >= 5 && credit < 8) {
+    return "Que voulez vous commander : 3/ Boisson Non Alcoolisé 4/ Sortir";
+  } else {
+    return "vous n'avez pas assez de credit";
+  }
+}
+// BOUCLE DEMANDE CHOIX CLIENT TANT QU IL Y A ARGENT
+let choix = "";
+while (credit >= 5 && choix != "4") {
+  choix = prompt(
+    "Que voulez vous commander : 1/ Plat 2/ Boisson Alcoolisée 3/ Boisson Non Alcoolisé 4/ Sortir"
+  );
+  alert(choix);
+
+  // PLAT
+  if (choix == "1") {
+    if (credit >= PLAT) {
+      credit = credit - PLAT;
+      alert(`Commande accepté voici votre plat il vous reste ${credit} euros`);
+    } else {
+      alert("Vous n'avez pas assez d'argent commandez autre chose.");
+    }
+  }
+
+  // BOISSON ALCOOL
+  else if (choix == "2") {
+    if (age < 18) {
+      alert("Vous n'êtes pas majeur vous ne pouvez pas commander d'alcool");
+    } else if (credit < BOISSON_ALCOOLISEE) {
+      alert("Vas au travail pour gagner plus d'argent !");
+    } else {
+      credit = credit - BOISSON_ALCOOLISEE;
+      alert(`A la tienne il vous reste ${credit} euros `);
+    }
+  }
+
+  // BOISSON NON ALCOOLISEE
+  else if (choix == "3") {
+    if (credit >= BOISSON_NON_ALCOOLISEE) {
+      credit = credit - BOISSON_NON_ALCOOLISEE;
+      alert(`Commande accepté voici votre soda il vous reste ${credit} euros `);
+    } else {
+      alert("Vous n'avez pas assez d'argent commandez autre chose.");
+    }
+  }
+
+  // TOUS LES AUTRES CAS
+  else {
+    alert("Veuillez réessayer");
+  }
+  alert(recupererChoixPossible(credit));
+}
